f01c74abf57b7ef71aca0b397db033ed06f16923
Jayhmz <Jayhmz10@gmail.com>
2022-11-03 14:18:26 +0100

    create json file for the read csv file

--- src/main/java/com/zuri/hng/internship/stage_two/task_1/App.java
+++ src/main/java/com/zuri/hng/internship/stage_two/task_1/App.java
@@ -1,9 +1,12 @@
 package com.zuri.hng.internship.stage_two.task_1;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
@@ -19,8 +22,16 @@ public class App
     	String csv = new BufferedReader(
         		new InputStreamReader(Objects.requireNonNull(isInputStream), StandardCharsets.UTF_8))
         		.lines().collect(Collectors.joining("\n"));
+    	
     	String json = CDL.toJSONArray(csv).toString(2);
     	System.out.println(json);
+    	
+    	try {
+			Files.write(Path.of("all-teams.json"), json.getBytes());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     	
     }
 }
