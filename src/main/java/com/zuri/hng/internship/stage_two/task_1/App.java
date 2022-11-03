33bc7e9263de23f1f047b5569cf0adae9165b616
Jayhmz <Jayhmz10@gmail.com>
2022-11-03 14:12:52 +0100

    initial commit

--- src/main/java/com/zuri/hng/internship/stage_two/task_1/App.java
+++ src/main/java/com/zuri/hng/internship/stage_two/task_1/App.java
@@ -1,10 +1,13 @@
 package com.zuri.hng.internship.stage_two.task_1;
 
+import java.io.InputStream;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        
+    	InputStream isInputStream = App.class.getResourceAsStream("all-teams.csv");
+    	System.out.println(isInputStream);
     	
     }
 }
